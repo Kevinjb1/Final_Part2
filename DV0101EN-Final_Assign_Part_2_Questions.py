7692c7f01a9a3788628ca8446767fead2b543e76
Kevin Barber <kevinjb2001@gmail.com>
2023-08-08 08:47:18 -0500

    adding working code

--- DV0101EN-Final_Assign_Part_2_Questions.py
+++ DV0101EN-Final_Assign_Part_2_Questions.py
@@ -59,7 +59,7 @@ app.layout = html.Div([
 #TASK 2.4: Creating Callbacks
 # Define the callback function to update the input container based on the selected statistics
 @app.callback(
-    Output(component_id='select year', component_property='disabled'),
+    Output(component_id='select-year', component_property='disabled'),
     Input(component_id='dropdown-statistics',component_property='value'))
 
 def update_input_container(selected_statistics):
@@ -72,8 +72,8 @@ def update_input_container(selected_statistics):
 # Define the callback function to update the input container based on the selected statistics
 @app.callback(
     Output(component_id='output-container', component_property='children'),
-    [Input(component_id='select year', component_property='value'), 
-    Input(component_id='dropdown-statistics', component_property='value')])
+    [Input(component_id='dropdown-statistics', component_property='value'), 
+    Input(component_id='select-year', component_property='value')])
 
 
 def update_output_container(selected_statistics,input_year ):
@@ -85,7 +85,7 @@ def update_output_container(selected_statistics,input_year ):
 
 #Plot 1 Automobile sales fluctuate over Recession Period (year wise)
         # use groupby to create relevant data for plotting
-        yearly_rec=recession_data.groupby('Year')['Automobile_sales'].mean().reset_index()
+        yearly_rec=recession_data.groupby('Year')['Automobile_Sales'].mean().reset_index()
         R_chart1 = dcc.Graph(
             figure=px.line(yearly_rec, 
                 x='Year',
@@ -105,15 +105,15 @@ def update_output_container(selected_statistics,input_year ):
         exp_rec= recession_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset_index()
         R_chart3 = dcc.Graph(figure=px.pie(exp_rec,
                                 values = 'Advertising_Expenditure',
-                                names='Vehicle_type',
+                                names='Vehicle_Type',
                                 title='Advertising based on Vehicle Type during Recessions'))
 
 # Plot 4 bar chart for the effect of unemployment rate on vehicle type and sales
         unemp_rec = recession_data.groupby(['Vehicle_Type','unemployment_rate'])['Automobile_Sales'].mean().reset_index()
         R_chart4 = dcc.Graph(figure=px.bar(unemp_rec,
                                 x='unemployment_rate',
-                                y='Automobile_Sales'),
-                                title='Effect of Unemployment on Auto Sales by Type')
+                                y='Automobile_Sales',
+                                title='Effect of Unemployment on Auto Sales by Type'))
 
 #TASK 2.6: Returning the graphs for displaying Recession data
         return [
@@ -165,4 +165,3 @@ def update_output_container(selected_statistics,input_year ):
 # Run the Dash app
 if __name__ == '__main__':
     app.run_server(port=8060)
-
